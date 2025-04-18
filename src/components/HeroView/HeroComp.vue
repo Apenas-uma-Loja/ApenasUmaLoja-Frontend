cae480f644ce387546858992e48e6b0a16dc1785
rumians <ruanfort12@gmail.com>
2025-04-17 23:49:20 -0300

    FEAT: Criaçao do infoCupom

--- src/components/HeroView/HeroComp.vue
+++ src/components/HeroView/HeroComp.vue
@@ -2,6 +2,7 @@
 import TitleDescription from './TitleDescription.vue';
 import ButtonAboutWant from './ButtonAboutWant.vue';
 import SideImage from './SideImage.vue';
+import InfoCupom from './InfoCupom.vue';
 </script>
 
 <template>
@@ -11,8 +12,9 @@ import SideImage from './SideImage.vue';
                 <div class="container-hero-title-description">
                     <TitleDescription />
                 </div>
-                <div class="container-hero-button-about-want">
+                <div class="container-hero-button-info">
                     <ButtonAboutWant />
+                    <InfoCupom />
                 </div>
             </div>
             <div class="container-right">
@@ -54,10 +56,15 @@ import SideImage from './SideImage.vue';
     background-color: rgba(139, 47, 24, 0.555);
 }
 
-.container-hero-button-about-want {
+.container-hero-button-info {
     width: 100%;    
     height: 40%;
     background-color: rgba(0, 0, 255, 0.466);
 }
 
+.container-hero-info-cupom {
+    width: 100%;
+    height: 50%;
+    background-color: rgba(217, 217, 233, 0.466);
+}
 </style>
