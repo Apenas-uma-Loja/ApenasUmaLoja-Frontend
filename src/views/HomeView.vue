8fcae2790a8443126e260563662981f787681b8a
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2025-04-25 21:47:19 -0300

    FEAT: adicionando produtos da semana à home view [REFS: #6]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,17 @@
 <script setup>
-// import TypeProductsComp from '@/components/TypeProducts/TypeProductsComp.vue';
-// import FooterComp from '../components/footer/FooterComp.vue';
-// import HeaderComp from '@/components/header/HeaderComp.vue';
-// import HeroComp from '@/components/HeroView/HeroComp.vue';
-// import ProductCard from '@/components/ProductsofWeek/ProductCard.vue';
+import TypeProductsComp from '@/components/TypeProducts/TypeProductsComp.vue';
+import FooterComp from '../components/footer/FooterComp.vue';
+import HeaderComp from '@/components/header/HeaderComp.vue';
+import HeroComp from '@/components/HeroView/HeroComp.vue';
 import ProductsComp from '@/components/ProductsofWeek/ProductsComp.vue';
 </script>
 
 <template>
   <main>
-    <ProductsComp />
-    <!-- <ProductCard /> -->
-    <!-- <HeaderComp />
+    <HeaderComp />
     <HeroComp />
     <TypeProductsComp />
-    <FooterComp /> -->
+    <ProductsComp />
+    <FooterComp />
   </main>
 </template>
